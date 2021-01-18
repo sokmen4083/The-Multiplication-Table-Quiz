2ca3e4123b95fc401a2404c0d62798e41ca720ac
Mehmet Sökmen <sokmen4083@gmail.com>
2021-01-18 20:34:03 +0100

    web-site is ready

--- end.js
+++ end.js
@@ -1,13 +1,13 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
-const mostRecentScore = localStorage.getItem("mostRecentScore");//localstorage ile scoru yazdıracagız
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
-  saveScoreBtn.disabled = !username.value;//burada kullanıcı ismi girilmemişse kaydet butonu actif olmasın tıklanmasın yapıldı
+  saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = e => {
@@ -19,8 +19,8 @@ saveHighScore = e => {
     name: username.value
   };
   highScores.push(score);
-  highScores.sort((a, b) => b.score - a.score);//burada alınan puanları sort ile siralıyoruz
-  highScores.splice(10);//splice ile burada 10 scordan fazlasını kesşyoruz
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(10);
   localStorage.setItem("highScores", JSON.stringify(highScores));
   window.location.assign("/");
 };
